0bd58b4f07e9a9f1f8b8cd2747cd13ed95d4b16b
TheAweSum1 <101042154+TheAweSum1@users.noreply.github.com>
2025-10-05 20:10:54 +0530

    Final clean-up of prog6

--- W1T1prog6/main.c
+++ W1T1prog6/main.c
@@ -1,17 +1,23 @@
+/*
+ * 6. Write a C program that demonstrates compound assignment operators
+ *    (i.e. +=, -=, *=, /=, %=) with an integer variable. Consider a
+ *    variable 'a' initialised to value 10.
+ */    
 #include <stdio.h>
 #include <stdbool.h>
 
 int main()
-{
+{   //declare a
     int a = 0;
+    //Take input of a from user
     printf("Initial value: ");
     scanf("%d", &a);
-    //Take input of initial value from user
+    //use of ternary operator like +=, -=, %= etc to add , subtract, modulus respectively the given value to a and store it back into a
     printf("After += 5: %d", (a+=5));
     printf("After -= 3: %d", (a-=3));
-    printf("After = 2: %d", (a=2));
+    printf("After *= 2: %d", (a*=2));
     printf("After /= 4: %d", (a/=4));
     printf("After %= 3: %d", (a%=3));
-    //use of ternary operator like +=, -=, %= etc to add , subtract, modulus respectively the given value to a and store it back into a
+    
     return 0;
 }
