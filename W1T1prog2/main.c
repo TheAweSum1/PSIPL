624f8105526e2807bd1093c25718b1b6ab09a88a
TheAweSum1 <101042154+TheAweSum1@users.noreply.github.com>
2025-10-05 09:05:09 +0530

    Final clean-up of prog2

--- W1T1prog2/main.c
+++ W1T1prog2/main.c
@@ -1,14 +1,26 @@
+/*
+* Write a C program that uses constant keyword. Define a constant for Pi and calculate the area of the circle
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-/* Write a C program tht uses constant keyword. Define a constant for Pi and calculate the areaof  circle
-*/
-const float pi = 3.1415926;
-float rad = 0.0;
-printf("Enter radius:");
-scanf("%f", &rad);
-float area = pi * rad * rad;
-printf(" Area of circle = %f", area);
+      // defining pi as a constant of type float
+      const float pi = 3.1415926;
+        
+      //initializing radius to 0
+      float rad = 0.0;
+        
+      //taking input from user
+      printf("Enter radius:");
+      scanf("%f", &rad);
+        
+      //calculating area of circle
+      float area = pi * rad * rad;
+        
+      //printing area of circle
+      printf(" Area of circle = %f", area);
+  
+      return 0;
 }
