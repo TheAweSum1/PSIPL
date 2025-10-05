944a5bc84ef40fb45942859b6216c7856ee9ec97
TheAweSum1 <101042154+TheAweSum1@users.noreply.github.com>
2025-10-05 09:22:28 +0530

    Final clean-up of prog4

--- W1T1prog4/main.c
+++ W1T1prog4/main.c
@@ -1,14 +1,25 @@
+/*
+ * 4. Write a C program to input two numbers and check which one is greateror if they are equal using ternary operator.
+ */
 #include <stdio.h>
 #include <stdbool.h>
 int main()
-{
+{   
+    //declare two numbers
     int num1, num2;
+    
+    //taking input of two numbers
     printf("Enter two numbers:");
-    scanf("%d %d", &num1, &num2); //Ask for two numbers and take two numbers num1 and num2 as input
+    scanf("%d %d", &num1, &num2); 
+    
+    // use ternary operator to store the greater of the two numbers in greaternum
+    int greaternum = (num1>num2) ? num1 :num2;
 
-    int greaternum = (num1>num2) ? num1 :num2; // use ternary operator to store the greater of the two numbers in greaternum
-    (num1 == num2) ? (printf("%d is equal to %d", num1, num2)) :(printf("%d is greater than %d", greaternum, (num1 + num2 - greaternum)));
     //use ternary operator to check if num1 is equal to num 2. If it is, then the fact is printed to the console, else the greater of the two is specified
+    (num1 == num2) ?
+    (printf("%d is equal to %d", num1, num2)) 
+    :
+    (printf("%d is greater than %d", greaternum, (num1 + num2 - greaternum)));
 
     return 0;
 
