547ee08b3fbf7adda8547813ff43ce8efabfd95c
TheAweSum1 <mhaskarcg@gmail.com>
2025-10-09 11:52:05 +0530

    feat: added few programs

---
+++ W1T2/assignment1_T2_prog1/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   int rollno, sub1, sub2, sub3;
+    printf("Enter Roll Number: ");
+    scanf("%d" , &rollno);
+    printf("Enter marks in 3 subjects: ");
+    scanf("%d %d %d",&sub1, &sub2, &sub3);
+    float total = 1.00*(sub1 + sub2 + sub3);
+    float avg = total/3;
+    float prcnt = avg;
+    char grade = ' ';
+    printf("\n --- Result --- \n");
+    printf("Roll No: %d",rollno);
+    if(rollno % 2 != 0)
+        printf(" (Odd)\n");
+    else
+        printf(" (Even)\n");
+    if(prcnt >= 75.00)
+        grade = 'A';
+    else if(prcnt >= 60.00)
+        grade = 'B';
+    else if(prcnt >= 50.00)
+        grade = 'C';
+    else if(prcnt >= 40.00)
+        grade = 'D';
+    else
+        grade = 'F';
+
+    printf("Marks: %d, %d, %d\n", sub1 , sub2 , sub3);
+    printf("Total: %.2f\n",total);
+    printf("Average: %.2f\n",avg);
+    printf("Percentage: %.2f%%\n", prcnt);
+    printf("Grade: %c \n",grade);
+
+
+    return 0;
+}
--- W2T1/assignment2_T1_prog9/main.c
+++ W2T1/assignment2_T1_prog9/main.c
@@ -2,9 +2,19 @@
 #include <stdlib.h>
 
 int main()
-{   int n;
+{
+    int n,t;
+    long fac = 1;
+
     printf("Enter a number to calculte its factorial:");
     scanf("%d", &n);
+    t = n;
+    while(n>1)
+    {
+        fac *= n;
+        n--;
+    }
 
+    printf("Factorial of %d is =  (%d!) = %d", t, t, fac);
     return 0;
 }
